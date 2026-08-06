0db0e7e3eb02201f98620334e2f6bebbf000bff2
prtkumar <prabhatkumar286266@gmail.com>
2026-08-06 17:57:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,3 +17,15 @@ room : "gate"
 };
 
 console.log("हमारा घर प्रारम्भ हो गया।");
+
+const World = {
+    state: "gate",
+    entered: false,
+    currentRoom: "gate"
+};
+
+function enterHome() {
+    World.entered = true;
+    World.state = "path";
+    console.log("घर का द्वार खुल गया।");
+}
